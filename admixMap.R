fbcc43ce3387626cb65aeaf79d439a601a62b650
yuek9 <61337841+yuek9@users.noreply.github.com>
2020-02-23 20:39:27 -0800

    Add files via upload
    Upload GENESIS source codes. Obtained from Github UW-GAC/GENESIS repository. 
    Reference: 
    Gogarten, S.M., Sofer, T., Chen, H., Yu, C., Brody, J.A., Thornton, T.A., Rice, K.M., and Conomos, M.P. (2019). Genetic association testing using the GENESIS R/Bioconductor package. Bioinformatics. doi:10.1093/bioinformatics/btz567.

---
+++ admixMap.R
@@ -0,0 +1,162 @@
+admixMap <- function(admixDataList,
+                     null.model,
+                     male.diploid=TRUE, genome.build=c("hg19", "hg38"),
+                     verbose = TRUE){
+
+    # if admixDataList is one file, convert to a list
+    if(!is.list(admixDataList)){
+        admixDataList <- list(admixDataList)
+    }
+
+    # how many ancestry components to be tested
+    v <- length(admixDataList)
+
+    # if admixDataList doesn't have names, assign them
+    if(is.null(names(admixDataList))){
+        names(admixDataList) <- paste("Anc",1:v,sep="")
+    }
+
+    # get sample index
+    if (is(admixDataList[[1]], "GenotypeIterator")) {
+        sample.index <- lapply(admixDataList, .sampleIndexNullModel, null.model)
+        if (!.listIdentical(sample.index)) stop("sample IDs do not match for all elements of admixDataList")
+        sample.index <- sample.index[[1]]
+    } else if (is(admixDataList[[1]], "SeqVarIterator")) {
+        sample.index <- lapply(admixDataList, .setFilterNullModel, null.model, verbose=verbose)
+        if (!.listIdentical(sample.index)) stop("sample IDs do not match for all elements of admixDataList")
+        sample.index <- sample.index[[1]]
+    } else {
+        stop("admixDataList must contain GenotypeIterator or SeqVarIterator objects")
+    }
+    n.samp <- length(sample.index)
+
+    # get variant information
+    var.info <- lapply(admixDataList, variantInfo, alleles=FALSE)
+    if (!.listIdentical(var.info)) stop("variants do not match for all elements of admixDataList")
+    
+    
+    # set up results matrix
+    # add in frequency of each ancestry at the SNP
+    res.list <- list()
+    nv <- c("n.obs")
+    if(v > 1){
+        for(i in 1:v){
+            nv <- append(nv, paste(names(admixDataList)[i],c(".freq",".Est",".SE"), sep=""))
+        }
+        nv <- append(nv, c("Joint.Stat", "Joint.pval"))
+    }else{
+        nv <- append(nv, c("freq", "Est", "SE", "Stat", "pval"))
+    }
+
+    
+    n.iter <- length(variantFilter(admixDataList[[1]]))
+    set.messages <- ceiling(n.iter / 100) # max messages = 100
+    b <- 1
+    iterate <- TRUE
+    while (iterate) {
+        var.info <- variantInfo(admixDataList[[1]], alleles=FALSE)
+        n.var <- nrow(var.info)
+
+        res <- matrix(NA, nrow=n.var, ncol=length(nv), dimnames=list(NULL, nv))
+        
+        # get local ancestry for the block
+        local <- array(NA, dim=c(n.samp,n.var,v)) # indices: scan, snp, ancestry
+        for(i in 1:v){
+            if (is(admixDataList[[1]], "GenotypeIterator")) {
+                local[,,i] <- getGenotypeSelection(admixDataList[[i]], scan=sample.index, order="selection", transpose=TRUE, use.names=FALSE, drop=FALSE)
+            } else {
+                local[,,i] <- refDosage(admixDataList[[i]], use.names=FALSE)[sample.index,,drop=FALSE]
+            }
+        }
+        if (any(is.na(local))) warning("missing values in local ancestry will produce NA output for this block")
+
+        # ancestral frequency
+        # matrix:  rows are SNPs, columns are ancestries
+        freq <- matrix(NA, nrow=n.var, ncol=v)
+        for(i in 1:v){
+            freq[,i] <- .alleleFreq(admixDataList[[i]], local[,,i], sample.index=sample.index,
+                                    male.diploid=male.diploid, genome.build=genome.build)$freq
+        }
+        col <- if (v > 1) paste(names(admixDataList),".freq", sep="") else "freq"
+        res[,col] <- freq
+        
+        # sample size
+        res[, "n.obs"] <- n.samp
+        
+        k <- ncol(null.model$model.matrix)
+        Ytilde <- null.model$Ytilde
+        sY2 <- sum(Ytilde^2)
+        
+        # perform regressions
+        if(v == 1){
+            local <- local[,,1]
+            Gtilde <- calcGtilde(null.model, local)
+            GPG <- colSums(Gtilde^2) # vector of G^T P G (for each SNP)
+            # filter monomorphic SNPs
+            GPG[freq[,1] == 0 | freq[,1] == 1] <- NA
+            beta <- as.vector(crossprod(Gtilde,Ytilde)/GPG)
+            Vbeta <- (sY2/GPG - beta^2)/(n.samp - k - 1) # RSS/GPG
+            Stat <- beta^2/Vbeta
+
+            res[,"Est"] <- beta
+            res[,"SE"] <- sqrt(Vbeta)
+            res[,"Stat"] <- Stat
+            res[,"pval"] <- pchisq(Stat, df=1, lower.tail=FALSE)      
+
+        }else{
+            Joint.Stat <- rep(NA, ncol(local))
+            Est <- matrix(NA, nrow=ncol(local), ncol=v)
+            SE <- matrix(NA, nrow=ncol(local), ncol=v)
+            for(g in 1:ncol(local)){
+                if(identical(local[,g,], local[,(g-1),])){
+                    Joint.Stat[g] <- Joint.Stat[g-1]
+                    Est[g,] <- Est[g-1,]
+                    SE[g,] <- SE[g-1,]                     
+                    next
+                }
+                # filter monomorphic or missing SNPs
+                if(any(freq[g,]==1) || sum(freq[g,]==0)){ next }
+                Gtilde <- calcGtilde(null.model, local[,g,])
+                GPG <- crossprod(Gtilde)
+                GPGinv <- tryCatch( chol2inv(chol(GPG)), error=function(e){TRUE})
+                # check that the error function above hasn't been called (which returns TRUE instead of the inverse matrix)
+                if(is.logical(GPGinv)){ next }
+                GPY <- crossprod(Gtilde, Ytilde)
+                betas <- crossprod(GPGinv, GPY)
+                RSS <- as.numeric((sY2 - crossprod(GPY,betas))/(n.samp - k - v))
+                Vbetas <- GPGinv*RSS
+
+                Est[g,] <- as.vector(betas)
+                SE[g,] <- as.vector(sqrt(diag(Vbetas)))
+                Joint.Stat[g] <- tryCatch( crossprod(betas,crossprod(GPG,betas))/RSS, error=function(e){NA})
+            } # g loop
+
+            # collect results
+            for(i in 1:v){
+                res[,paste(names(admixDataList)[i],".Est", sep="")] <- Est[,i]
+                res[,paste(names(admixDataList)[i],".SE", sep="")] <- SE[,i]
+            }
+            res[,"Joint.Stat"] <- Joint.Stat
+            res[,"Joint.pval"] <- pchisq(Joint.Stat, df=v, lower.tail=FALSE)
+        } # else
+
+        # results data frame
+        res <- cbind(var.info, res)
+        
+        res.list[[b]] <- res
+
+        if (verbose & n.iter > 1 & b %% set.messages == 0) {
+            message(paste("Iteration", b , "of", n.iter, "completed"))
+        }
+        b <- b + 1
+        for (i in 1:v) {
+            if (is(admixDataList[[i]], "GenotypeIterator")) {
+                iterate <- GWASTools::iterateFilter(admixDataList[[i]])
+            } else {
+                iterate <- SeqVarTools::iterateFilter(admixDataList[[i]], verbose=verbose)
+            }
+        }
+    }
+    
+    do.call(rbind, res.list)
+}
---
+++ fitNullModel.R
@@ -0,0 +1,175 @@
+
+setGeneric("fitNullModel", function(x, ...) standardGeneric("fitNullModel"))
+
+setMethod("fitNullModel",
+          "data.frame",
+          function(x, outcome,
+                   covars = NULL,
+                   cov.mat = NULL,
+                   group.var = NULL,
+                   family = "gaussian",
+                   start = NULL,
+                   AIREML.tol = 1e-4,
+                   max.iter = 100,
+                   EM.iter = 0,
+                   drop.zeros = TRUE,
+                   verbose = TRUE) {
+              
+              desmat <- createDesignMatrix(x, outcome, covars, group.var)
+
+              # if there was missing data, need to subset cov.mat
+              if (!is.null(cov.mat)) {
+                  .checkRownames(cov.mat, x)
+                  if (nrow(desmat$X) < nrow(x)) {
+                      ind <- which(rownames(x) %in% rownames(desmat$X))
+                      cov.mat <- .covMatSubset(cov.mat, ind)
+                  }
+              }
+              
+              .fitNullModel(y=desmat$y, X=desmat$X, covMatList=cov.mat,
+                            group.idx=desmat$group.idx, family=family,
+                            start=start, AIREML.tol=AIREML.tol,
+                            max.iter=max.iter, EM.iter=EM.iter,
+                            drop.zeros=drop.zeros, verbose=verbose)
+          })
+
+setMethod("fitNullModel",
+          "AnnotatedDataFrame",
+          function(x, outcome,
+                   covars = NULL,
+                   cov.mat = NULL,
+                   group.var = NULL,
+                   sample.id = NULL,
+                   ...) {
+              
+              x <- pData(x)
+              rownames(x) <- x$sample.id
+
+              if (!is.null(cov.mat)) {
+                  .checkSampleId(cov.mat, x)
+              }
+              
+              ## subset data.frame and cov.mat for selected samples
+              if (!is.null(sample.id)) {
+                  stopifnot(all(sample.id %in% x$sample.id))
+                  ind <- x$sample.id %in% sample.id
+                  x <- x[ind,]
+                  if (!is.null(cov.mat)) {
+                      ind <- which(.covMatNames(cov.mat) %in% sample.id)
+                      cov.mat <- .covMatSubset(cov.mat, ind)
+                  }
+              }
+
+              ## reorder data.frame to match cov.mat
+              if (!is.null(cov.mat)) {
+                  ind <- match(.covMatNames(cov.mat), rownames(x))
+                  x <- x[ind,]
+              }
+              
+              nm <- fitNullModel(x, outcome, covars, cov.mat, group.var, ...)
+              nm$sample.id <- rownames(nm$model.matrix)
+              nm
+          })
+
+setMethod("fitNullModel",
+          "SeqVarData",
+          function(x, ...) {
+              fitNullModel(sampleData(x), ...)
+          })
+
+setMethod("fitNullModel",
+          "ScanAnnotationDataFrame",
+          function(x, ...) {
+              class(x) <- "AnnotatedDataFrame"
+              varLabels(x)[varLabels(x) == "scanID"] <- "sample.id"
+              fitNullModel(x, ...)
+          })
+
+setMethod("fitNullModel",
+          "GenotypeData",
+          function(x, ...) {
+              fitNullModel(getScanAnnotation(x), ...)
+          })
+
+
+nullModelInvNorm <- function(null.model, cov.mat = NULL,
+                             norm.option = c("by.group", "all"),
+                             rescale = c("none", "model", "residSD"),
+                             AIREML.tol = 1e-4, 
+                             max.iter = 100, EM.iter = 0,
+                             verbose = TRUE) {
+
+    updateNullModOutcome(null.model, covMatList=cov.mat, rankNorm.option=norm.option,
+                         rescale=rescale, AIREML.tol=AIREML.tol, 
+                         max.iter=max.iter, EM.iter=EM.iter,
+                         verbose=verbose)
+}
+
+
+
+## function to get sample.ids from cov.mat rownames and/or column names
+## error if they don't match
+.covMatNames <- function(cov.mat) {
+    if (is.list(cov.mat) & length(cov.mat) == 1) {
+        cov.mat <- cov.mat[[1]]
+    }
+    if (!is.list(cov.mat)) {
+        names <- dimnames(cov.mat)
+        if (!is.null(names[[1]]) & !is.null(names[[2]]) & any(names[[1]] != names[[2]])) {
+            stop("dimnames of cov.mat should be identical")
+        }
+        if (!is.null(names[[1]])) {
+            return(names[[1]])
+        } else {
+            return(names[[2]])
+        }
+    } else {
+        rows <- lapply(cov.mat, rownames)
+        cols <- lapply(cov.mat, colnames)
+        if (!.listIdentical(rows)) stop("dimnames of cov.mat should be identical")
+        if (!.listIdentical(cols)) stop("dimnames of cov.mat should be identical")
+        if (!is.null(rows[[1]])) {
+            return(rows[[1]])
+        } else {
+            return(cols[[1]])
+        }
+    }
+}
+
+
+## need to subset cov.mat in case of missing data
+## don't re-order in case we have a block diagonal matrix
+.covMatSubset <- function(cov.mat, index) {
+    .subset <- function(x, index) {
+        if (identical(index, 1:nrow(x))) {
+            return(x)
+        } else {
+            return(x[index,index])
+        }
+    }
+    if (!is.list(cov.mat)) {
+        .subset(cov.mat, index)
+    } else {
+        cov.mat <- return(lapply(cov.mat, .subset, index))
+    }
+}
+
+
+## match rownames between cov.mat and data frame
+.checkRownames <- function(cov.mat, x) {
+    nms <- .covMatNames(cov.mat)
+    if (!is.null(nms) & !all(nms == rownames(x))) {
+        stop("dimnames of cov.mat must match rownames of x")
+    }
+}
+
+
+## match sample.id between cov.mat and data frame
+.checkSampleId <- function(cov.mat, x) {
+    nms <- .covMatNames(cov.mat)
+    if (is.null(nms)) {
+        stop("provide sample.id in rownames and/or colnames of cov.mat")
+    } else if (!all(nms %in% x$sample.id)) {
+        stop("all sample names in dimnames of cov.mat must be present in x$sample.id")
+    }
+}
