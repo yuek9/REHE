4902b5732ae371d4e8bdd1b184b46a50c14a00d7
yuek9 <61337841+yuek9@users.noreply.github.com>
2020-05-30 23:52:46 -0700

    Delete readfile.py

--- readfile.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def readfile(pre, n, p, library_scale, theta, option, z_scale, end):
-    filename = pre+str(int(n))+'_'+str(int(p))+'_'+str(float(library_scale))+'_'+str(float(theta))+'_'+option+'_'+str(float(z_scale))+end
-    print(filename)
-    return np.load(filename)
